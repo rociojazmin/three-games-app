8b4a884b120c22e1b3996822e5839da0f5dd8226
rociojazmin <rociomiramont@gmail.com>
2023-02-02 15:24:36 -0300

    adding css landsacpe

--- www/js/jslib.js
+++ www/js/jslib.js
@@ -47,3 +47,23 @@ function turnOffSound() {
     sound = false;
     document.getElementById("cartel-sonido").classList.add("nodisp");
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
